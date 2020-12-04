4f4a61f903fd766e93acc730fa03db11d53ab456
llwei <lianwei1994@163.com>
2020-12-04 14:23:27 +0800

    修改 dubbo代码

--- dubbo/dubbo-dubbo/dubbo-demo/dubbo-demo-xml/dubbo-demo-xml-monitor/src/main/java/org/apache/dubbo/demo/monitor/SimpleMonitorService.java
+++ dubbo/dubbo-dubbo/dubbo-demo/dubbo-demo-xml/dubbo-demo-xml-monitor/src/main/java/org/apache/dubbo/demo/monitor/SimpleMonitorService.java
@@ -3,17 +3,21 @@ package org.apache.dubbo.demo.monitor;
 import org.apache.dubbo.common.URL;
 import org.apache.dubbo.monitor.MonitorService;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class SimpleMonitorService implements MonitorService {
+    private URL statistics;
+
     @Override
     public void collect(URL statistics) {
         System.out.println("监控1。。。");
+        this.statistics = statistics;
     }
 
     @Override
     public List<URL> lookup(URL query) {
         System.out.println("监控2。。。");
-        return null;
+        return Arrays.asList(statistics);
     }
 }
