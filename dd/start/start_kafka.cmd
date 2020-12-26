eb38e58926e1fffeaeb6300c59fdaaacd1bbb799
llwei <lianwei1994@163.com>
2020-12-26 18:48:49 +0800

    修改

---
+++ dd/start/start_kafka.cmd
@@ -0,0 +1,2 @@
+cd D:\linux\kafka_2.12-2.5.0\bin\windows
+kafka-server-start.bat ../../config/server.properties
---
+++ dd/start/start_rocket_broker.cmd
@@ -0,0 +1,2 @@
+cd D:\linux\rocketmq-all-4.7.1-bin-release\bin
+mqbroker.cmd -n localhost:9876 autoCreateTopicEnable=true
