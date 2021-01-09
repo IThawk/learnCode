7c9e35a37dc07787ed2157178fc0469505fc6d32
llwei <lianwei1994@163.com>
2021-01-09 15:21:34 +0800

    添加redisson

---
+++ redis/redisson/src/main/java/com/ithawk/demo/redisson/Test.java
@@ -0,0 +1,23 @@
+package com.ithawk.demo.redisson;
+
+import org.redisson.Redisson;
+import org.redisson.api.*;
+import org.redisson.config.Config;
+import org.redisson.config.TransportMode;
+import reactor.core.publisher.Mono;
+
+public class Test {
+    public static void main(String[] args) throws InterruptedException {
+        Config config = new Config();
+        config.useSingleServer()
+                //可以用"rediss://"来启用SSL连接
+                .setAddress("redis://192.168.56.101:6379");
+        RedissonClient redisson = Redisson.create(config);
+        RLock lock = redisson.getLock("myLock");
+        lock.lock();
+        Thread.sleep(50000);
+        lock.unlock();
+
+
+    }
+}
