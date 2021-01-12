79e2350353b00596470cd8bd6e4f9769b23ea15f
llwei <lianwei1994@163.com>
2021-01-12 12:07:22 +0800

    添加redisson

---
+++ redis/redisson-springboot/src/main/java/com/ithawk/demo/springboot/redis/cache/SpringBootRedisCacheApplication.java
@@ -0,0 +1,13 @@
+package com.ithawk.demo.springboot.redis.cache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SpringBootRedisCacheApplication {
+    public static void main(String[] args) throws InterruptedException {
+        SpringApplication.run(SpringBootRedisCacheApplication.class, args);
+
+    }
+}
