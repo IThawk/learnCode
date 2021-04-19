f6e82c297564edc763cf82fc5abac233101aa919
llwei <lianwei1994@163.com>
2021-04-20 07:43:45 +0800

    添加netty 源码已经mybatis相关代码

--- docker/start_mysql.sh
+++ docker/start_mysql.sh
@@ -1,1 +1,1 @@
-sudo docker run -p 3306:3306 -e MYSQL_ROOT_PASSWORD="123456" -d mysql:5.7
+docker run --name mysql -p 3306:3306 -v /mydata/mysql:/var/lib/mysql --privileged=true -e MYSQL_ROOT_PASSWORD="123456" -d mysql:5.7
