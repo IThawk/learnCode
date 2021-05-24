f128aaff9e592b8efd2a75b190fef47ba3e5f7dc
llwei <lianwei1994@163.com>
2021-05-24 18:12:17 +0800

    添加elk 搭建，使用说明

--- elasticsearch/supergo-logstash/src/main/java/com/ithawk/demo/elasticsearch/logstash/LogstashApplication.java
+++ elasticsearch/supergo-logstash/src/main/java/com/ithawk/demo/elasticsearch/logstash/LogstashApplication.java
@@ -1,4 +1,4 @@
-package com.supergo;
+package com.ithawk.demo.elasticsearch.logstash;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
--- elasticsearch/supergo-logstash/src/main/java/com/ithawk/demo/elasticsearch/logstash/controller/LogController.java
+++ elasticsearch/supergo-logstash/src/main/java/com/ithawk/demo/elasticsearch/logstash/controller/LogController.java
@@ -1,4 +1,4 @@
-package com.supergo.controller;
+package com.ithawk.demo.elasticsearch.logstash.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
