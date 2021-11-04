6c7a855977dae386d8000ad214caf957aba0cf50
llwei <lianwei1994@163.com>
2021-11-04 20:42:57 +0800

    add code

---
+++ javaStructures/leecode-learn/src/main/java/com/ithawk/learn/middle/LargestNumber.java
@@ -0,0 +1,8 @@
+package com.ithawk.learn.middle;/**  
+    * 
+    * @projectName javaStructures
+    * @description: TODO
+    * @author ithawk
+    * @date 2021/11/416:51
+    */public class LargestNumber {
+}
---
+++ javaStructures/leecode-learn/src/main/java/com/ithawk/learn/simple/MajorityElement.java
@@ -0,0 +1,8 @@
+package com.ithawk.learn.simple;/**  
+    * 
+    * @projectName javaStructures
+    * @description: TODO
+    * @author ithawk
+    * @date 2021/11/416:23
+    */public class MajorityElement {
+}
