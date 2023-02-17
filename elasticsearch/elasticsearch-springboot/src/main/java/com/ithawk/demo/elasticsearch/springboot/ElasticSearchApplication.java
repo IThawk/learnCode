99ab3ccacada410e926224e19bd4bb892fa706c7
IThawk <lianwei1994@163.com>
2023-02-17 10:04:15 +0800

    elasticSearch

--- elasticsearch/elasticsearch-springboot/src/main/java/com/ithawk/demo/elasticsearch/springboot/ElasticSearchApplication.java
+++ elasticsearch/elasticsearch-springboot/src/main/java/com/ithawk/demo/elasticsearch/springboot/ElasticSearchApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 @SpringBootApplication(exclude =
         {
@@ -16,4 +18,10 @@ public class ElasticSearchApplication {
         SpringApplication.run(ElasticSearchApplication.class, args);
     }
 
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer sourcesPlaceholderConfigurer(){
+        PropertySourcesPlaceholderConfigurer sourcesPlaceholderConfigurer = new PropertySourcesPlaceholderConfigurer();
+//        sourcesPlaceholderConfigurer.setIgnoreUnresolvablePlaceholders(true);
+        return sourcesPlaceholderConfigurer;
+    }
 }
--- elasticsearch/elasticsearch-springboot/src/main/java/com/ithawk/demo/elasticsearch/springboot/config/ElasticSearchConfig.java
+++ elasticsearch/elasticsearch-springboot/src/main/java/com/ithawk/demo/elasticsearch/springboot/config/ElasticSearchConfig.java
@@ -4,15 +4,30 @@ import org.apache.http.HttpHost;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
-import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.elasticsearch.ElasticsearchRestClientProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @Configuration
 public class ElasticSearchConfig {
 
+    private List<String> uris;
 
+    @Value("${spring.elasticsearch.rest.test}")
+    public String test;
+
+    @Value("#{'${spring.elasticsearch.rest.urls}'.split(',')}")
+    public List<String> uris1;
+    @Autowired
+    ElasticsearchRestClientProperties elasticsearchRestClientProperties;
     public static final RequestOptions COMMON_OPTIONS;
+
     static {
         RequestOptions.Builder builder = RequestOptions.DEFAULT.toBuilder();
 //        builder.addHeader("Authorization", "Bearer " + TOKEN);
@@ -24,13 +39,24 @@ public class ElasticSearchConfig {
 
     /**
      * 自定义elasticsearch的查询client
+     *
      * @return
      */
     @Bean
-    public RestHighLevelClient esRestClient(){
-        RestHighLevelClient client = new RestHighLevelClient(
-                RestClient.builder(
-                        new HttpHost("192.168.56.101", 9200, "http")));
-        return client;
+    @ConditionalOnClass(ElasticsearchRestClientProperties.class)
+    public RestHighLevelClient esRestClient() {
+        uris = elasticsearchRestClientProperties.getUris();
+        List<HttpHost> httpHosts = IntStream.range(0, uris.size())
+                .mapToObj(i -> {
+                    String[] s = uris.get(i).split("://");
+                    String scheme = s[0];
+                    String[] s1 = s[1].split(":");
+                    String host = s1[0];
+                    int port = Integer.parseInt(s1[1]);
+                    return new HttpHost(host, port, scheme);
+                }).collect(Collectors.toList());
+
+        return new RestHighLevelClient(
+                RestClient.builder(httpHosts.stream().toArray(HttpHost[]::new)));
     }
 }
